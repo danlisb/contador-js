70dfbd77f4c7ddd7e38e02877328a3e52adf19e5
Daniel Lisboa <86170130+danlisb@users.noreply.github.com>
2022-06-23 23:29:35 -0300

    Add files via upload

---
+++ Loops/assets/js/script.js
@@ -0,0 +1,12 @@
+let currentNumber = 0;
+var currentNumberWrapper = document.getElementById('currentNumber');
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
